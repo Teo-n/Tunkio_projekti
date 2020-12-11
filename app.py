75af470c54f4c5a45ac7bffe9039dbe0795480e8
Teo-n <teo.nikulainen@edu.turkuamk.fi>
2020-12-11 17:53:38 +0200

    Fixed small error

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ api.add_resource(ReservationResource, '/reservations/<int:reservation_id>')
 api.add_resource(ReservationPublishResource, '/reservations/<int:reservation_id>/publish')
 
 api.add_resource(WorkspaceListResource, '/workspaces')
-api.add_resource(WorkspaceResource, '/workspaces/<int:workspace_id')
+api.add_resource(WorkspaceResource, '/workspaces/<int:workspace_id>')
 api.add_resource(WorkspacePublishResource, '/workspaces/<int:workspace_id>/publish')
 
 if __name__ == '__main__':
