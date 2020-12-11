852ef4df23e8f3bf95ab075d9723de0c1bc26138
Teo-n <teo.nikulainen@edu.turkuamk.fi>
2020-12-11 17:52:19 +0200

    Updated app.py
    Updated app.py to work with newly created files.

--- app.py
+++ app.py
@@ -1,91 +1,24 @@
-from flask import Flask, jsonify, request
-from http import HTTPStatus
+from flask import Flask
+from flask_restful import Api
 
-# creating app
+from resources.client import ClientListResource, ClientResource, ClientPublishResource
+from resources.reservation import ReservationListResource, ReservationResource, ReservationPublishResource
+from resources.workspace import WorkspaceListResource, WorkspaceResource, WorkspacePublishResource
+
 app = Flask(__name__)
+api = Api(app)
 
-# creating table
-clients = {
-    'id',
-    'name',
-    'email'
-}
+api.add_resource(ClientListResource, '/clients')
+api.add_resource(ClientResource, '/clients/<int:client_id>')
+api.add_resource(ClientPublishResource, '/clients/<int:client_id>/publish')
 
-# creating table
-workspaces = {
-    'id',
-    'building',
-    'roomType',
-    'floor',
-    'roomNumber'
-}
+api.add_resource(ReservationListResource, '/reservations')
+api.add_resource(ReservationResource, '/reservations/<int:reservation_id>')
+api.add_resource(ReservationPublishResource, '/reservations/<int:reservation_id>/publish')
 
-# creating table
-reservations = {
-    'id',
-    'date',
-    'time',
-    'client'
-}
-
-
-# CLIENTS TABLE
-# GET response returns all clients
-@app.route('/clients', methods=['GET'])
-def get_clients():
-    return jsonify({'data': clients})
-
-
-# GET response returns one client by id
-@app.route('/clients/<int:client_id>', methods=['GET'])
-def get_client(client_id):
-    client = next((client for client in clients if client['id'] == client_id), None)
-
-    if client:
-        return jsonify(client)
-# if client is not found, error message is shown
-    return jsonify({'message': 'Cannot find client'}), HTTPStatus.NOT_FOUND
-
-
-# POST response returns name and email values
-@app.route('/clients', methods=['POST'])
-def create_client():
-    data = request.get_json()
-
-    name = data.get('name')
-    email = data.get('email')
-
-    client = {
-        'id': + 1,
-        'name': name,
-        'email': email
-    }
-
-    clients.append(client)
-# return created client (has JSON format and HTTP status)
-    return jsonify(client), HTTPStatus.CREATED
-
-
-# PUT response edits a client by id
-@app.route('/clients/<int:client_id>', methods=['PUT'])
-def update_client(client_id):
-    client = next((client for client in clients if client['id'] == client_id), None)
-
-    if not client:
-        return jsonify({'message': 'Cannot find client'}), HTTPStatus.NOT_FOUND
-
-# If the client is found, the update function will be performed
-    data = request.get_json()
-
-    client.update(
-        {
-            'name': data.get('name'),
-            'email': data.get('email')
-        }
-    )
-
-    return jsonify(client)
-
+api.add_resource(WorkspaceListResource, '/workspaces')
+api.add_resource(WorkspaceResource, '/workspaces/<int:workspace_id')
+api.add_resource(WorkspacePublishResource, '/workspaces/<int:workspace_id>/publish')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
