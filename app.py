c06e3eddb5d2be7f2ab9ef1ab441471bc24e04cc
Anna <AL13NZ@users.noreply.github.com>
2020-12-13 13:01:35 +0200

    Added User and resource info to app

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_restful import Api
+from extensions import db, jwt
+from resources.user import UserListResource, UserResource
+from resources.token import TokenResource
 
 from resources.client import ClientListResource, ClientResource, ClientPublishResource
 from resources.reservation import ReservationListResource, ReservationResource, ReservationPublishResource
@@ -19,6 +22,21 @@ api.add_resource(ReservationPublishResource, '/reservations/<int:reservation_id>
 api.add_resource(WorkspaceListResource, '/workspaces')
 api.add_resource(WorkspaceResource, '/workspaces/<int:workspace_id>')
 api.add_resource(WorkspacePublishResource, '/workspaces/<int:workspace_id>/publish')
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app,db)
+    jwt.init_app(app)
+
+def register_resources(app):
+    api = Api(app)
+
+    api.add_resource(UserListResource, '/users')
+    api.add_resource(UserResource, '/users/<string:username>')
+
+    api.add_resource(TokenResource, '/token')
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
